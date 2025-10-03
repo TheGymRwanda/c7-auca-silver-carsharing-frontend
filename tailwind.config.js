bde0aac98585d02dea55a2ca92adf55009a858f0
Gdev <125574259+gisachris@users.noreply.github.com>
2025-10-03 10:57:53 +0200

    feat: include light and dark primary color variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#265E78',
+        primary: {
+          light: '#3E7591',
+          dark: '#265E78'
+        },
         ghost: {
           light: '#B8B8B8',
           dark: '#d1d1d1',
