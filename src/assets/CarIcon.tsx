5318d0bdc940515f59fa20f2670b949601426beb
Prom004 <izerepromise004@gmail.com>
2025-10-07 11:11:26 +0200

    feat: customising icons

--- src/assets/CarIcon.tsx
+++ src/assets/CarIcon.tsx
@@ -13,7 +13,7 @@ export default function CarIcon({ className, color = '#059669' }: CarIconProps):
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      stroke="currentColor"
+      stroke={color}
       className={className}
     >
       <path
